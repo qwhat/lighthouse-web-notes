7fb7d23ee676d0d205c86774f7aa7c73d23b5db0
qwhat <justinmao@live.ca>
2019-01-10 23:16:02 +0000

    adding array_sorting.js

---
+++ project/Week_1/Day_4/array_sorting.js
@@ -0,0 +1,20 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+students.sort(function (a, b) {
+  var nameA = a.name.toUpperCase()
+  var nameB = b.name.toUpperCase()
+  if (nameA < nameB){
+    return -1;
+  }
+  else if (nameA > nameB) {
+    return 1
+  }
+  else {
+    return b.age - a.age
+  }
+})
+console.log(students)
